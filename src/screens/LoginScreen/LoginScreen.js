9404113fd6e4b3d774d2580f2cf2138e288656c9
goldipl <mgodlewski92@gmail.com>
2022-07-20 21:49:49 +0200

    styling login screen

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import Button from "../../components/atoms/Button";
 
 import { useLogin } from "../../hooks/useLogin";
 import ROUTES from "../../constants/routes";
+
+const styles = StyleSheet.create({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    paddingHorizontal: 32,
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: 600,
+    marginBottom: 16,
+  },
+});
 
 function LoginScreen({ navigation }) {
   const login = useLogin();
@@ -15,17 +31,11 @@ function LoginScreen({ navigation }) {
   }
 
   return (
-    <View
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100%",
-      }}
-    >
-      <Text>LoginScreen</Text>
-      <Button title={"go back"} onPress={() => navigation.goBack()} />
+    <View style={styles.root} >
+      <Text style={styles.text}>Login Screen</Text>
       <Button title={"Dashboard"} onPress={onSubmit} />
+      <br></br>
+      <Button title={"Go back"} onPress={() => navigation.goBack()} />
     </View>
   );
 }
